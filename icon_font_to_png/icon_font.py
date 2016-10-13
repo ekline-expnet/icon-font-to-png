532d7fdc6a27044bf767f711a5c6cfd2648e7635
ekline-expnet <ekline@expression.net>
2016-10-13 11:42:18 -0400

    modified export function to handle an optional wrapper icon

--- icon_font_to_png/icon_font.py
+++ icon_font_to_png/icon_font.py
@@ -74,42 +74,36 @@ class IconFont(object):
 
         return sorted_icons, common_prefix
 
-    def export_icon(self, icon, size, color='black', scale='auto',
-                    filename=None, export_dir='exported'):
+    def scale_font(self, ttf, icon, size, scale):
         """
-        Exports given icon with provided parameters.
+        Scales a font to make sure the font fits inside boudary
 
-        If the desired icon size is less than 150x150 pixels, we will first
-        create a 150x150 pixels image and then scale it down, so that
-        it's much less likely that the edges of the icon end up cropped.
-
-        :param icon: valid icon name
-        :param filename: name of the output file
+        :param ttf: file path to the ttf file
+        :param icon: valid icon name for graphic to scale
         :param size: icon size in pixels
-        :param color: color name or hex value
         :param scale: scaling factor between 0 and 1,
                       or 'auto' for automatic scaling
-        :param export_dir: path to export directory
-        """
-        org_size = size
-        size = max(150, size)
 
-        image = Image.new("RGBA", (size, size), color=(0, 0, 0, 0))
-        draw = ImageDraw.Draw(image)
-
-        if scale == 'auto':
-            scale_factor = 1
-        else:
-            scale_factor = float(scale)
-
-        font = ImageFont.truetype(self.ttf_file, int(size * scale_factor))
-        width, height = draw.textsize(self.css_icons[icon], font=font)
+        :returns the scaled font, width, height, bbox of image
 
         # If auto-scaling is enabled, we need to make sure the resulting
         # graphic fits inside the boundary. The values are rounded and may be
         # off by a pixel or two, so we may need to do a few iterations.
         # The use of a decrementing multiplication factor protects us from
         # getting into an infinite loop.
+        """
+        Helper function to scale a font
+        if scale == 'auto':
+            scale_factor = 1
+        else:
+            scale_factor = float(scale)
+
+        image = Image.new("RGBA", (size, size), color=(0, 0, 0, 0))
+        draw = ImageDraw.Draw(image)
+
+        font = ImageFont.truetype(ttf, int(size * scale_factor))
+        width, height = draw.textsize(self.css_icons[icon], font=font)
+
         if scale == 'auto':
             iteration = 0
             factor = 1
@@ -129,12 +123,59 @@ class IconFont(object):
                 iteration += 1
                 if iteration % 2 == 0:
                     factor *= 0.99
-
-        draw.text((float(size - width) / 2, float(size - height) / 2),
-                  self.css_icons[icon], font=font, fill=color)
-
         # Get bounding box
         bbox = image.getbbox()
+        return font, width, height, bbox
+
+    def export_icon(self, icon, size, color='black', scale='auto',
+                    filename=None, export_dir='exported', wrapper_icon=''):
+        """
+        Exports given icon with provided parameters.
+
+        If the desired icon size is less than 150x150 pixels, we will first
+        create a 150x150 pixels image and then scale it down, so that
+        it's much less likely that the edges of the icon end up cropped.
+
+        :param icon: valid icon name
+        :param filename: name of the output file
+        :param size: icon size in pixels
+        :param color: color name or hex value
+        :param scale: scaling factor between 0 and 1,
+                      or 'auto' for automatic scaling
+        :param export_dir: path to export directory
+        :param wrapper_icon: valid icon name to use as an outer wrapper
+        """
+        if wrapper_icon == '':
+            self.export_icon_with_wrapper(icon=icon, size=size,
+                color=color, scale=scale, filename=filename,
+                export_dir=export_dir)
+        else:
+            self.export_icon_with_wrapper(icon=icon, size=size,
+                color=color, scale=scale, filename=filename,
+                export_dir=export_dir, wrapper_icon=wrapper_icon)
+
+    def export_icon_no_wrapper(self, icon, size, color='black', scale='auto',
+                    filename=None, export_dir='exported'):
+        """
+        Exports given icon with provided parameters.
+
+        If the desired icon size is less than 150x150 pixels, we will first
+        create a 150x150 pixels image and then scale it down, so that
+        it's much less likely that the edges of the icon end up cropped.
+
+        :param icon: valid icon name
+        :param filename: name of the output file
+        :param size: icon size in pixels
+        :param color: color name or hex value
+        :param scale: scaling factor between 0 and 1,
+                      or 'auto' for automatic scaling
+        :param export_dir: path to export directory
+        """
+        org_size = size
+        size = max(150, size)
+
+        font, width, height, bbox = self.scale_font(ttf=ttf, icon=icon,
+            size=size, scale=scale)
 
         # Create an alpha mask
         image_mask = Image.new("L", (size, size), 0)
@@ -150,6 +191,76 @@ class IconFont(object):
 
         if bbox:
             icon_image = icon_image.crop(bbox)
+
+        border_w = int((size - (bbox[2] - bbox[0])) / 2)
+        border_h = int((size - (bbox[3] - bbox[1])) / 2)
+
+        # Create output image
+        out_image = Image.new("RGBA", (size, size), (0, 0, 0, 0))
+        out_image.paste(icon_image, (border_w, border_h))
+
+        # If necessary, scale the image to the target size
+        if org_size != size:
+            out_image = out_image.resize((org_size, org_size), Image.ANTIALIAS)
+
+        # Make sure export directory exists
+        if not os.path.exists(export_dir):
+            os.makedirs(export_dir)
+
+        # Default filename
+        if not filename:
+            filename = icon + '.png'
+
+        # Save file
+        out_image.save(os.path.join(export_dir, filename))
+
+    def export_icon_with_wrapper(self, icon, size, wrapper_icon,
+        color='black', scale='auto', filename=None, export_dir='exported'):
+        """
+        Exports given icon with provided parameters.
+
+        If the desired icon size is less than 150x150 pixels, we will first
+        create a 150x150 pixels image and then scale it down, so that
+        it's much less likely that the edges of the icon end up cropped.
+
+        :param icon: valid icon name
+        :param filename: name of the output file
+        :param size: icon size in pixels
+        :param color: color name or hex value
+        :param scale: scaling factor between 0 and 1,
+                      or 'auto' for automatic scaling
+        :param export_dir: path to export directory
+        :param wrapper_icon: valid icon name to use as an outer wrapper
+        """
+        if scale == 'auto':
+            scale_factor = 1.0
+        else:
+            scale_factor = float(scale)
+
+        full_scale = scale_factor
+        scale_factor = float(scale_factor / 2.0)
+
+        font, width, height, bbox = self.scale_font(ttf=self.ttf_file,
+            icon=icon, size=size, scale=scale_factor)
+        font2, width2, height2, bbox2 = self.scale_font(ttf=self.ttf_file,
+            icon=wrapper_icon, size=size, scale=full_scale)
+
+        # Create an alpha mask
+        image_mask = Image.new("L", (size, size), 0)
+        draw_mask = ImageDraw.Draw(image_mask)
+
+        # Draw the icons on the mask
+        draw_mask.text((float(size - width2) / 2, float(size - height2) / 2),
+                       self.css_icons[wrapper_icon], font=font2, fill=255)
+        draw_mask.text((float(size - width) / 2, float(size - height) / 2),
+                       self.css_icons[icon], font=font, fill=255)
+
+        # Create a solid color image and apply the mask
+        icon_image = Image.new("RGBA", (size, size), color)
+        icon_image.putalpha(image_mask)
+
+        if bbox2:
+            icon_image = icon_image.crop(bbox2)
 
         border_w = int((size - (bbox[2] - bbox[0])) / 2)
         border_h = int((size - (bbox[3] - bbox[1])) / 2)
